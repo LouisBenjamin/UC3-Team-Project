8ecd6d4b64b542e4b4b5716f16e4a88eba85c697
HarishJayasankar <46975286+HarishJayasankar@users.noreply.github.com>
2019-02-09 00:09:50 +0530

    Index page
    #6 # 10 #11 #23 #25

---
+++ index.php
@@ -0,0 +1,35 @@
+ <?php
+ 		include  'core/init.php';
+
+ ?>	
+ <html>
+	<head>
+		<title>tato</title>
+	</head>
+<body>
+
+
+<div class="wrapper">
+
+
+	
+		<div class="content-left">
+			<!-- Log In Section -->
+			<div class="login-wrapper">
+			  <?php include 'includes/login.php';?>
+			</div><!-- log in wrapper end -->
+
+			<!-- SignUp Section -->
+			<div class="signup-wrapper">
+			   <?php include 'includes/signup.php';?>
+			</div>
+			<!-- SIGN UP wrapper end -->
+
+		
+	</div><!-- main container end -->
+
+</div><!-- ends wrapper -->
+</body>
+</html>
+
+  
