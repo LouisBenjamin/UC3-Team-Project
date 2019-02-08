c8281da9a1ac22760909617fa83e1b1cb4d64b5b
HarishJayasankar <46975286+HarishJayasankar@users.noreply.github.com>
2019-02-09 00:06:56 +0530

    It contain global variables,Include files
    #6 #10 #11  #23 # 24 #25

---
+++ core/init.php
@@ -0,0 +1,10 @@
+ <?php
+    include 'databases/connection.php';
+    include  'classes/user.php';
+
+    global $pdo;
+    session_start();
+    $getUser=new User($pdo);
+
+    define("Base_URL", "http://localhost/tato/"); 
+ ?>
