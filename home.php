382803cc03f42928656e1f2c7d902f32baf447be
Louis-Benjamin Durocher <benfit34@apexsciences.com>
2019-02-10 16:11:14 -0500

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 <!DOCTYPE HTML> 
  <html>
 	<head>
-		<title>Tweety</title>
+		<title>Tato</title>
 		  <meta charset="UTF-8" />
 		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>  
  	  	  <link rel="stylesheet" href="assets/css/style-complete.css"/> 
