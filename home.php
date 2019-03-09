8d73ed225f0fb92a899f6520dc36801a1ec668d5
Tash <tashfianaharinproma@hotmail.com>
2019-03-09 05:41:07 -0500

    #53 Comments and updated html code.
    Made few changes in the html code as well as added few comments.

--- home.php
+++ home.php
@@ -31,106 +31,117 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-    <meta charset="UTF-8">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tato</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <!--        <link rel="stylesheet" href="css/main.css">-->
-    <style>
-        .well p {
-            text-align: left;
-        }
-        .well img {
-            margin: 20px 0;
-        }
-    </style>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-2.2.1.min.js" defer></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" defer></script>
+    <style>
+        .well p { text-align: left; }
+        .well img { margin: 20px 0; }
+        #a { background-color: lightblue; }
+        #b { background-color: lightgreen; }
+        #userName { color: black; font-weight: bold; font-size: medium; }
+        #tatosfeed { font-weight: bold; font-size: large; padding: 11.75px; 
+                     background-color:lightgray; border-radius: 5px; } 
+    </style>
+
 </head>
+
 <body>
+    
+    <!--Navigation bar-->
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Tato</a>
+            </div>
 
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Tato</a>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+
+                    <!-- redirect to Home page-->
+                    <li class="active" id="post-new"><a href="home.php">Home</a></li>
+
+                    <!-- redirect to -->
+                    <li><a href="#" id="post-list">About</a></li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+
+                    <!-- redirect to My profile page-->
+                    <li><a href="profile.php" id="categories-editor">
+                        <span class="glyphicon glyphicon-user"></span> MyAccount</a></li>
+
+                    <!-- redirect to login out page -->
+                    <li><a href="includes/logout.php">Logout</a></li>
+                </ul>
+            </div>
+
         </div>
+    </nav>
 
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active" id="post-new"><a href="home.php">Home</a></li>
+    
+    <div class="container text-center">
 
-                <!-- redirect to -->
-                <li><a href="#" id="post-list">About</a></li>
-            </ul>
+        <div class="row">
 
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="profile.php" id="categories-editor"><span class="glyphicon glyphicon-user"></span> My
-                        Account</a></li>
-
-                <!-- redirect to -->
-                <li><a href="includes/logout.php">Logout</a></li>
-            </ul>
-        </div>
-
-    </div>
-</nav>
-
-<div class="container text-center">
-    <div class="row">
-        <div class="col-sm-3 well">
-            <div class="well" style="margin-bottom: 0">
-                <h5><a href="profile.php"><?= $user_data->username; ?></a></h5>
-                <img src="data:image/jpeg;base64,<?php echo $user_data->profile_image; ?>" class="img-circle"
-                     height="65" width="65" alt="Avatar">
-                <p><b> Followers: <?= $user_data->fan_count; ?></b></p>
-                <p><b> Following: <?= $user_data->idol_count; ?></b></p>
+            <!-- To display the sidebar-->
+            <div class="col-sm-3 well">
+                
+                <div class="well" style="margin-bottom: 0">
+                  
+                    <img src="data:image/jpeg;base64,<?php echo $user_data->profile_image; ?>" class="img-circle"
+                         height="80" width="80" alt="Profilepic"/>
+                     <div>
+                        <a href="profile.php" id="userName">
+                           <?= $user_data->username; ?>     
+                        </a>
+                     </div>
+                     <br>
+                     <p id="a"><b> Followers: <?= $user_data->fan_count; ?></b></p>
+                     <p id="b"><b> Following: <?= $user_data->idol_count; ?></b></p>
+                     <br>
+                </div>
 
             </div>
-            <!--            <div class="well">-->
-            <!--                <p><a href="#">Interests</a></p>-->
-            <!--                <p>-->
-            <!--                    <span class="label label-default">Category</span>-->
-            <!--                    <span class="label label-primary">Category</span>-->
-            <!--                    <span class="label label-success">Category</span>-->
-            <!--                    <span class="label label-info">Category</span>-->
-            <!--                    <span class="label label-warning">Category</span>-->
-            <!--                    <span class="label label-danger">Category</span>-->
-            <!--                </p>-->
-            <!--            </div>-->
-        </div>
-        <div class="col-sm-7">
 
-            <div class="row">
+            <!-- To submit and display posts-->
+            <div class="col-sm-7">
+                <div class="row">
+                    <div class="col-md-12">
+                      <div class="well" style="margin-bottom: 0">
+                        <h4 style="text-align: left">Leave a Tato</h4>
+                        <form role="form" method="post">
+                            <div class="form-group">
+                                 <textarea class="form-control" name="tato_status" rows="3" required></textarea>
+                                  <?php
+                                      if (isset($error)) {
+                                        echo '<div class="span-fp-error">' . $error . '</div>';
+                                      }
+                                  ?>
+                            </div>
+                            <div style="text-align: right">
+                                <button type="submit" name="tato_submit" class="btn btn-success">Submit</button>
+                            </div>
+                        </form>
 
-                <div class="col-md-12">
-                    <div class="well" style="margin-bottom: 0">
-                    <h4 style="text-align: left">Leave a Tato</h4>
-                    <form role="form" method="post">
-                        <div class="form-group">
-                            <textarea class="form-control" name="tato_status" rows="3" required></textarea>
-                          <?php
-                          if (isset($error)) {
-                            echo '<div class="span-fp-error">' . $error . '</div>';
-                          }
-                          ?>
-                        </div>
-                        <div style="text-align: left">
-                            <button type="submit" name="tato_submit" class="btn btn-success">Submit</button>
-                        </div>
-                    </form>
-                      <form method="post" enctype="multipart/form-data" style="text-align: left">
-                        <input type="file" name="image" id="image"/>
-                        <input type="submit" value="Upload" name="image_submit" id="image-upload"/>
-                    </form>
-                </div>
-                    <div style="text-align: left"> <?php $getTato->showTatoes(); ?> </div>
-
+                        <!-- To upload picture-->
+                        <form method="post" enctype="multipart/form-data" style="text-align: left">
+                            <input type="file" name="image" id="image"/>
+                            <input type="submit" value="Upload" name="image_submit" id="image-upload"/>
+                        </form>
+                      </div>
+                      <br>
+                      <p id="tatosfeed"> Tatos Feed </p>
+                      <div style="text-align: left"> <?php $getTato->showTatoes(); ?> </div>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
-</div>
 
 </body>
 </html>
